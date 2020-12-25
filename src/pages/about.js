78406b12b9331e5b2961cb74adacf042e38cff83
Aviran Ohana <aviran3369@gmail.com>
2020-12-25 21:59:12 +0200

    change avout page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,11 +3,11 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 
 export default ({ data }) => (
-  <Layout>
+  <React.Fragment>
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
     <Link to="/">Back to home</Link>
-  </Layout>
+  </React.Fragment>
 )
 
 export const query = graphql`
